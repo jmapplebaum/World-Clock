ff5e4f669a71d929b736562d1659d7c168bbc8c7
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-12 20:08:15 -0600

    Added H1 and footer, created separate JS and CSS files

---
+++ index.js
@@ -0,0 +1,11 @@
+function showCurrentTime(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+let countrySelect = document.querySelector("#countries");
+countrySelect.addEventListener("change", showCurrentTime);
