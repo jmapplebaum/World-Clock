05c35cb4aa5deee43583cdb90c9dd761b6c6ee39
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-15 17:50:46 -0600

    Added updateTime function

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ function showCurrentTime(event) {
 }
 let countrySelect = document.querySelector("#countries");
 countrySelect.addEventListener("change", showCurrentTime);
+
+//
+function updateTime() {
+  let londonElement = document.querySelector("#London");
+  let londonDate = londonElement.querySelector(".date");
+  let londonTime = londonElement.querySelector(".time");
+  let londonFormat = moment().tz("Europe/London");
+
+  londonDate.innerHTML = londonFormat.format("MMMM Do YYYY");
+  londonTime.innerHTML = londonFormat.format("h:mm:ss [<small>]A[</small>]");
+
+  let telAvivElement = document.querySelector("#Tel-Aviv");
+  let telAvivDate = telAvivElement.querySelector(".date");
+  let telAvivTime = telAvivElement.querySelector(".time");
+  let telAvivFormat = moment().tz("Israel");
+
+  telAvivDate.innerHTML = telAvivFormat.format("MMMM Do YYYY");
+  telAvivTime.innerHTML = telAvivFormat.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
